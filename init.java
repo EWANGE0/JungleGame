dd895b2d2a0371df989fc4f2912ac46adb88960f
Haiqi Wang <evangelinewang@Gelines-Magical-Box-2.local>
2024-01-16 11:52:10 -0500

    adding a new init.java

---
+++ init.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package JungleGame;
+
+/**
+ *
+ * @author evangelinewang
+ */
+public interface init 
+{
+    public int x00 = 70;
+    public int x0 = 105;
+    public int y0 = 100;
+    public int row = 10;
+    public int column = 9;
+    public int chesssize = 67;
+    public int size = 76;
+    
+    
+}
